a5934030116c9eef6ef0fc546d4c0828b089632e
swapnil2597 <swapnil.khare25@gmail.com>
2020-07-03 22:41:38 +0530

    Components part 2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,17 +1,31 @@
 import React, { Component } from "react";
-import Menu from "./MenuComponent";
+import { View } from "react-native";
+
 import { DISHES } from "../shared/dishes";
+import Menu from "./MenuComponent";
+import Dishdetail from "./DishdetailComponent";
+import { SafeAreaView } from "react-native";
 
 class Main extends Component {
  constructor(props) {
   super(props);
   this.state = {
    dishes: DISHES,
+   selectedDish: null,
   };
  }
 
+ onDishSelect(dishId) {
+  this.setState({ selectedDish: dishId });
+ }
+
  render() {
-  return <Menu dishes={this.state.dishes} />;
+  return (
+   <SafeAreaView>
+    <Menu dishes={this.state.dishes} onPress={(dishId) => this.onDishSelect(dishId)} />
+    <Dishdetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+   </SafeAreaView>
+  );
  }
 }
 
--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -4,7 +4,7 @@ import { ListItem } from "react-native-elements";
 
 function Menu(props) {
  const renderMenuItem = ({ item, index }) => {
-  return <ListItem key={index} title={item.name} subtitle={item.description} hideChevron={true} leftAvatar={{ source: require("./images/uthappizza.png") }} />;
+  return <ListItem key={index} title={item.name} subtitle={item.description} hideChevron={true} onPress={() => props.onPress(item.id)} leftAvatar={{ source: require("./images/uthappizza.png") }} />;
  };
 
  return <FlatList data={props.dishes} renderItem={renderMenuItem} keyExtractor={(item) => item.id.toString()} />;
